f905c9ce6e8300fdd58f0ab499d5e716fc2d12fe
El Alami Idriss <elalami.developer.1991@gmail.com>
2022-04-23 18:41:11 +0000

    add copy overlay animation

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Palette {...seedColors[3]} />
+        <Palette {...seedColors[0]} />
       </div>
     );
   }
--- src/components/BoxColor.js
+++ src/components/BoxColor.js
@@ -3,14 +3,35 @@ import { CopyToClipboard } from "react-copy-to-clipboard";
 import "./BoxColor.css";
 
 class BoxColor extends Component {
+  state = { copied: false };
+
+  handleCopyChange = () => {
+    this.setState({ copied: true }, () =>
+      setTimeout(() => this.setState({ copied: false }), 1500)
+    );
+  };
+
   render() {
     const { background, name } = this.props;
+    const { copied } = this.state;
 
     return (
-      <CopyToClipboard text={background}>
+      <CopyToClipboard text={background} onCopy={this.handleCopyChange}>
         <div className="BoxColor" style={{ background }}>
+          <div
+            className={`copy-overlay ${copied && "show"}`}
+            style={{ background }}
+          ></div>
+
+          <div className={`copy-text ${copied && "show"}`}>
+            <h2>copied !!</h2>
+            <p>{background}</p>
+          </div>
+
           <p className="box-color_text">{name}</p>
+
           <span className="see-more">more</span>
+
           <button className="copy-btn">copy</button>
         </div>
       </CopyToClipboard>
