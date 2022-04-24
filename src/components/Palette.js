3437200ad1362c2952717d5ce0ba921293cfcb5d
El Alami Idriss <elalami.developer.1991@gmail.com>
2022-04-24 16:20:52 +0000

    Add snackBar

--- src/components/Palette.js
+++ src/components/Palette.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import Snackbar from "@mui/material/Snackbar";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
 
 import "./Palette.css";
 
@@ -6,26 +9,27 @@ import NavBar from "./NavBar";
 import BoxColor from "./BoxColor";
 
 class Palette extends Component {
-  state = { level: 500, format: "hex" };
+  state = { level: 500, format: "hex", openSnackbar: true };
 
   changeLevel = (level) => {
     this.setState({ level });
   };
 
   changeFormat = (format) => {
-    this.setState({ format });
+    this.setState({ format, openSnackbar: true });
+  };
+
+  closeSnackBar = () => {
+    this.setState({ openSnackbar: false });
   };
 
   render() {
     const { palette } = this.props;
-    const { level, format } = this.state;
+    const { level, format, openSnackbar } = this.state;
 
-    const colorBoxes = palette.colors[level].map((color) => {
-      console.log(format);
-      return <BoxColor background={color[format]} name={color.name} />;
-    });
-
-    console.log(format);
+    const colorBoxes = palette.colors[level].map((color) => (
+      <BoxColor background={color[format]} name={color.name} />
+    ));
 
     return (
       <div className="Palette">
@@ -37,6 +41,30 @@ class Palette extends Component {
         />
         <div className="palette-colors">{colorBoxes}</div>
         <footer>OK</footer>
+
+        {/* Snack Bar for format*/}
+        <Snackbar
+          anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+          open={openSnackbar}
+          autoHideDuration={2000}
+          onClose={this.closeSnackBar}
+          message={
+            <span id="message-id" className="snack-bar-message">
+              Format Changed to {format.toUpperCase()}
+            </span>
+          }
+          ContentProps={{ "aria-describedby": "message-id" }}
+          action={
+            <IconButton
+              onClick={this.closeSnackBar}
+              color="inherit"
+              key="close"
+              aria-label="close"
+            >
+              <CloseIcon />
+            </IconButton>
+          }
+        />
       </div>
     );
   }
