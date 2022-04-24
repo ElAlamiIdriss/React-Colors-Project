28e0224b47951ac8edd2d0dd3738374f2b0528b0
El Alami Idriss <elalami.developer.1991@gmail.com>
2022-04-24 12:47:58 +0000

    add unstyled slider colors

--- src/App.js
+++ src/App.js
@@ -2,17 +2,15 @@ import React, { Component } from "react";
 import "./App.css";
 
 import { generatePalette } from "./helpers/ColorHelpers";
+import seedColors from "./seedColors";
 
 import Palette from "./components/Palette";
-import seedColors from "./seedColors";
 
 class App extends Component {
   render() {
-    console.log(generatePalette(seedColors[4]));
-
     return (
       <div className="App">
-        <Palette {...seedColors[4]} />
+        <Palette palette={generatePalette(seedColors[4])} />
       </div>
     );
   }
--- src/components/Palette.js
+++ src/components/Palette.js
@@ -1,16 +1,35 @@
 import React, { Component } from "react";
+import Slider from "rc-slider";
+
 import "./Palette.css";
+import "rc-slider/assets/index.css";
 
 import BoxColor from "./BoxColor";
 
 class Palette extends Component {
+  state = { level: 500 };
+
+  changeLevel = (level) => {
+    this.setState({ level });
+  };
+
   render() {
-    const colorBoxes = this.props.colors.map((color) => (
-      <BoxColor background={color.color} name={color.name} />
+    const { palette } = this.props;
+    const { level } = this.state;
+
+    const colorBoxes = palette.colors[level].map((color) => (
+      <BoxColor background={color.hex} name={color.name} />
     ));
 
     return (
       <div className="Palette">
+        <Slider
+          min={100}
+          max={900}
+          step={100}
+          value={level}
+          onChange={this.changeLevel}
+        />
         <h1 className="palette-title">Palette</h1>
         <div className="palette-colors">{colorBoxes}</div>
         <footer>OK</footer>
